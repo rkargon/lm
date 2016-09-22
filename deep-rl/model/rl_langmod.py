ed484b17460494119e2f35cca477d5b252871aa7
Sidd Karamcheti <sidd.karamcheti@gmail.com>
2016-09-22 00:51:16 -0400

    Add model init, basic train code

--- deep-rl/model/rl_langmod.py
+++ deep-rl/model/rl_langmod.py
@@ -2,12 +2,44 @@
 rl_langmod.py
 
 Core model definition file for a Deep-Q Network Language Model. The state-space input consists of
-a bag-of-words context vector, and the action space input consists of a one-hot vector corresponding
-to the target word (word following the context).
+a bag-of-words context vector, and the action space input consists of a hot-encoded vector
+corresponding to the target word (word following the context).
 """
+import tensorflow as tf
+
 
 class RLangmod():
-    def __init__(self):
+    def __init__(self, vocab_size):
         """
+        Instantiate the RLangmod model, with the necessary parameters.
 
+        :param vocab_size: Size of the state/action space vocabulary (for bag-of-words vectors).
+        """
+        self.vocab_size = vocab_size
+
+        # Setup Placeholders
+        self.state_input = tf.placeholder(tf.int32, shape=[None, self.vocab_size])  # Bag of Words
+        self.action_input = tf.placeholder(tf.int32, shape=[None])                  # Hot Encoding
+
+        # Instantiate Network Parameters
+        self.instantiate_weights()
+
+        # Get state-space embeddings
+        self.state_emb = self.get_state()
+
+        # Get action embedding
+        self.action_emb = self.get_action()
+
+    def instantiate_weights(self):
+        """
+        Initialize and build all the necessary network parameters.
+        """
+        pass
+
+    def get_state(self):
+        """
+        Given the state-input bag of words vector, perform a forward pass through the
+        state-embedding network, in order to create the state-embedding.
+
+        :return: Returns Tensor of shape [None, embedding_size]
         """
--- deep-rl/train.py
+++ deep-rl/train.py
@@ -3,7 +3,12 @@ train.py
 
 Core training file for the Deep-Q Network Language Model.
 """
+from model.rl_langmod import RLangmod
 import tensorflow as tf
+
+FLAGS = tf.app.flags.FLAGS
+tf.flags.DEFINE_integer('vocabulary_size', 10000, "Size of the language vocabulary.")
+tf.flags.DEFINE_integer('embedding_size', 100, "Size of intermediate embeddings.")
 
 
 def main(_):
@@ -11,6 +16,20 @@ def main(_):
     Main function, loads and vectorizes the data, instantiates the network variables, and runs the
     training process.
     """
+    # Perform data preprocessing here
+    # TODO
+
+    with tf.Session() as sess:
+        # Instantiate Network
+        print 'Building Network!'
+        rlangmod = RLangmod(FLAGS.vocabulary_size)
+
+        # Create a saver.
+        saver = tf.train.Saver(tf.all_variables())
+
+        # Initialize all variables
+        print "Initializing Variables"
+        sess.run(tf.initialize_all_variables())
 
 
 if __name__ == "__main__":
