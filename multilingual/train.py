de892f5a88417977f7e9a6b153a1fc0f42cbd1bd
Raphael Kargon <raphael.kargon@gmail.com>
2016-12-08 21:09:31 -0500

    refactored code, added code to examine embeddings

--- multilingual/train.py
+++ multilingual/train.py
@@ -13,9 +13,18 @@ import numpy as np
 import tensorflow as tf
 
 from model.lm import LM_RNN
-from preprocessor.reader import load_test_data, load_train_data, read
+from preprocessor.reader import load_test_data, load_train_data, read, init_vocab, basic_tokenizer
+
+ALL_LANGUAGES = ['en', 'es', 'fr']
+LANGUAGE_NAMES = {'en': 'English', 'fr': 'French', 'es': 'Spanish', 'de': 'German'}
+# TODO this is sadness
+# SRC_FILES = {'en': 'data/raw/english.en', 'es': 'data/raw/spanish.es', 'fr': 'data/raw/french.fr'}
+SRC_FILES = {'en': 'data-icml14/cs-en/1m-mono/all_en.in', 'es': 'data/raw/spanish.es', 'fr': 'data-icml14/en-fr/1m-mono/all_fr.in'}
 
 FLAGS = tf.app.flags.FLAGS
+# training data parameters
+tf.app.flags.DEFINE_string('train_langs', ' '.join(ALL_LANGUAGES), 'languages to use for training')
+tf.app.flags.DEFINE_string('test_langs', ' '.join(ALL_LANGUAGES), 'languages to use for testing')
 tf.app.flags.DEFINE_string('train_dir', 'data/train/', 'Path to training files.')
 tf.app.flags.DEFINE_string('val_dir', 'data/val/', 'Path to validation files.')
 tf.app.flags.DEFINE_string('test_dir', 'data/test/', 'Path to training files.')
@@ -25,78 +34,112 @@ tf.app.flags.DEFINE_boolean('reprocess_data', False,
                             'whether to re-process raw input data, or use pickled processed inputs. (Uses pickled '
                             'inputs by default.)')
 
+# vocab size parameters
 tf.app.flags.DEFINE_integer('vocab_size', 40000, 'Size of the vocabularies.')
+#TODO make these percentages
 tf.app.flags.DEFINE_integer('train_size', 1000000, 'Number of training examples.')
-tf.app.flags.DEFINE_integer('val_size', 50000, 'Number of validation examples.')
-tf.app.flags.DEFINE_integer('test_size', 100000, 'Number of test examples.')
+# tf.app.flags.DEFINE_integer('val_size', 50000, 'Number of validation examples.')
+tf.app.flags.DEFINE_integer('val_size', 0, 'Number of validation examples.')
+# tf.app.flags.DEFINE_integer('test_size', 100000, 'Number of test examples.')
+tf.app.flags.DEFINE_integer('test_size', 75000, 'Number of test examples.')
 
+# model architecture parameters
 tf.app.flags.DEFINE_integer('window_size', 5, 'Size of the fixed context window.')
 tf.app.flags.DEFINE_integer('rnn_steps', 20, 'Size of the fixed context window.')
 tf.app.flags.DEFINE_integer('embedding_size', 50, 'Size of hidden embedding representations.')
 tf.app.flags.DEFINE_integer('hidden_size', 512, 'Size of the hidden layer.')
 
+# training hyperparameters
 tf.app.flags.DEFINE_float('learning_rate', 0.0001, 'Learning rate for Adam Optimizer.')
 tf.app.flags.DEFINE_integer('batch_size', 200, 'Size of the batch size.')
 tf.app.flags.DEFINE_integer('num_epochs', 50, 'Number of epochs for training.')
 tf.app.flags.DEFINE_integer('eval_every', 200, 'Print statistics every eval_every batches.')
 tf.app.flags.DEFINE_float('dropout_prob', 0.5, 'Dropout keep probability.')
 
-LANGUAGES = ['en', 'es', 'fr']
-LANGUAGE_NAMES = {'en': 'English', 'fr': 'French', 'es': 'Spanish', 'de': 'German'}
-SRC_FILES = {'en': 'data/raw/english.en', 'es': 'data/raw/spanish.es', 'fr': 'data/raw/french.fr'}
+tf.app.flags.DEFINE_boolean('skip_training', False, 'Whether to skip training of the model. False by default.')
+tf.app.flags.DEFINE_string('load_model', None, 'Path to a saved Tensorflow dump of a trained model to restore.')
 
 
 def main(_):
     """
     Main training function, loads and vectorizes data, then runs the training process.
     """
+    # lists of language IDs corresponding to the languages to be used for training and testing
+    train_languages = FLAGS.train_langs.split(None)
+    test_languages = FLAGS.test_langs.split(None)
+    all_used_languages = list(set(train_languages + test_languages))
+
     if FLAGS.reprocess_data:
         print "Processing raw data"
-        for lang_id in LANGUAGES:
+        for lang_id in all_used_languages:
             read(lang_id, SRC_FILES[lang_id], FLAGS.vocab_size, FLAGS.rnn_steps, FLAGS.train_size,
                  FLAGS.val_size, FLAGS.test_size)
+
     with tf.Session() as sess:
         # Instantiate Network
         print "Building Network"
-        langmod = LM_RNN(LANGUAGES, FLAGS.embedding_size, FLAGS.rnn_steps, FLAGS.hidden_size, FLAGS.hidden_size,
-                         {l: FLAGS.vocab_size for l in LANGUAGES}, FLAGS.learning_rate)
+        langmod = LM_RNN(all_used_languages, FLAGS.embedding_size, FLAGS.rnn_steps, FLAGS.hidden_size,
+                         FLAGS.hidden_size,
+                         {l: FLAGS.vocab_size for l in all_used_languages}, FLAGS.learning_rate)
 
         # Create a saver.
         saver = tf.train.Saver(tf.all_variables())
 
-        # Initialize all variables
-        print "Initializing variables"
-        sess.run(tf.initialize_all_variables())
-        bsz = FLAGS.batch_size
+        # Load trained model, or initialize all variables randomly
+        if FLAGS.load_model is not None:
+            saver.restore(sess, FLAGS.load_model)
+        else:
+            print "Initializing variables"
+            sess.run(tf.initialize_all_variables())
 
-        print "Loading training data..."
+        # print "Loading training data..."
+        train_data = {lang_id: load_train_data(lang_id) for lang_id in train_languages}
+        loss_data = {lang_id: [] for lang_id in train_languages}
+        test_data = {lang_id: load_test_data(lang_id) for lang_id in test_languages}
+        test_losses = {lang_id: [] for lang_id in test_languages}
 
-        # TODO test data for every language?
-        train_data = {lang_id: load_train_data(lang_id) for lang_id in LANGUAGES}
-        test_data = {'en': load_test_data('en')}
-        loss_data = {lang_id: [] for lang_id in LANGUAGES}
-        test_losses = []
+        if not FLAGS.skip_training:
+            # Run n epochs of French/English/Spanish With Interleaving Batches
+            print 'Training for %d epochs...' % FLAGS.num_epochs
+            for i in range(FLAGS.num_epochs):
+                run_network(sess, langmod, data=train_data, perplexity_log=loss_data, languages=train_languages,
+                            train=True,
+                            eval_perplexity=True)
 
-        # Run n epochs of French/English/Spanish With Interleaving Batches
-        print 'Starting Training'
-        for i in range(FLAGS.num_epochs):
-            run_network(sess, langmod, data=train_data, perplexity_log=loss_data, train=True, eval_perplexity=True)
+                # Evaluate on Test Data
+                test_perplexity = run_network(sess, langmod, data=test_data, perplexity_log=None,
+                                              languages=test_languages)
+                for lang_id in test_languages:
+                    test_losses[lang_id].append(test_perplexity[lang_id])
+                    print 'Epoch %d/%d, %s Test Perplexity: %f' % (
+                        i + 1, FLAGS.num_epochs, LANGUAGE_NAMES[lang_id], test_perplexity[lang_id])
 
-            # Evaluate on Test Data
-            test_perplexity = run_network(sess, langmod, data=test_data, perplexity_log=None, languages=["en"])
-            test_losses.append(test_perplexity["en"])
-            print 'Epoch %d/%d, Test Perplexity: %f' % (i + 1, FLAGS.num_epochs, test_perplexity["en"])
+                # Save model
+                checkpoint_path = os.path.join(FLAGS.log_dir, 'model.ckpt')
+                saver.save(sess, checkpoint_path, FLAGS.train_size // FLAGS.batch_size)
 
-            # Save model
-            checkpoint_path = os.path.join(FLAGS.log_dir, 'model.ckpt')
-            saver.save(sess, checkpoint_path, FLAGS.train_size // bsz)
+            for ld in loss_data.keys():
+                print '%s: %s' % (LANGUAGE_NAMES[ld], loss_data[ld])
+            print 'Test:', test_losses
 
-        for ld in loss_data.keys():
-            print '%s: %s' % (LANGUAGE_NAMES[ld], loss_data[ld])
-        print 'Test:', test_losses
+        # Evaluate on Test Data
+        # print 'Evaluating on test data...'
+        # test_perplexity = run_network(sess, langmod, data=test_data, perplexity_log=None, languages=test_languages)
+        # for lang_id in test_languages:
+        #     test_losses[lang_id].append(test_perplexity[lang_id])
+        #     print 'Loaded model, %s Test Perplexity: %f' % (LANGUAGE_NAMES[lang_id], test_perplexity[lang_id])
+
+        # load vocab dictionary
+        dictionaries = {lang_id: init_vocab(lang_id, vocab_size=FLAGS.vocab_size) for lang_id in all_used_languages}
+
+        # Set up REPL to explore trained model
+        import code
+        vars = locals().copy()
+        vars.update(globals())
+        code.interact(local=vars)
 
 
-def run_network(sess, langmod, data, perplexity_log=None, languages=LANGUAGES, batch_size=FLAGS.batch_size, train=False,
+def run_network(sess, langmod, data, perplexity_log=None, languages=None, batch_size=FLAGS.batch_size, train=False,
                 eval_perplexity=False):
     """
     Runs a neural network for one epoch of the given data set. This can be used either for training or for testing.
@@ -106,7 +149,8 @@ def run_network(sess, langmod, data, perplexity_log=None, languages=LANGUAGES, b
     :param data: The given data on which to run, as a dictionary of tuples {lang_id: (inputs, outputs)}
     :param perplexity_log: If not None, a dictionary mapping each language id to a list to which perplexity values
     will be appended. None by default
-    :param languages: A list of language id's on which to run the network. By default uses global variable LANGUAGES
+    :param languages: A list of language id's on which to run the network. By default uses language IDs present in
+    the 'data' dictionary.
     :param batch_size: The batch size with which to pass data to the network.
     :param train: Whether or not to run a training step for each batch. False by default. If True, model parameters
     are updated, and dropout is used during training.
@@ -114,6 +158,8 @@ def run_network(sess, langmod, data, perplexity_log=None, languages=LANGUAGES, b
     the returned value will also be the perplexity of the last eval_size batches.
     :return: The perplexity of the network on the data, as a dictionary {lang_id: perplexity}
     """
+    if languages is None:
+        languages = data.keys()
     start_time = time.time()
     counter = 0
     total_losses = {lang_id: 0.0 for lang_id in languages}
@@ -148,6 +194,28 @@ def run_network(sess, langmod, data, perplexity_log=None, languages=LANGUAGES, b
             start_time = time.time()
 
     return {lang_id: np.exp(total_losses[lang_id] / counter) for lang_id in languages}
+
+
+def word_distance(sess, model, dictionaries, lang, w1, w2):
+    v1 = embed_lookup(sess, model, dictionaries, lang, w1)
+    v2 = embed_lookup(sess, model, dictionaries, lang, w2)
+    return np.linalg.norm(v1 - v2)
+
+
+def embed_lookup(sess, model, dictionaries, lang, word):
+    return sess.run(model.embeddings[lang])[dictionaries[lang][0][word]]
+
+
+def embed_sentence(sess, model, dictionaries, lang, sentence):
+    words = basic_tokenizer(sentence)
+    vocab = dictionaries[lang][0]
+    tokens = [vocab[w] for w in words]
+    gru_state = sess.run(model.gru_cell.zero_state(batch_size=1, dtype=tf.float32))
+    for t in tokens:
+        feed_dict = {model.inputs[lang]: [t]}
+        gru_state, sess.run([model.gru_stats[lang], model.intermediate_embeddings[lang]], feed_dict=feed_dict)
+
+    pass
 
 
 if __name__ == "__main__":
